dbdcb66d91bdab0a7103efed1ffa0800c58d6a4d
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2015-02-10 10:59:10 -0800

    Added Mojio model

---
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Mojio.java
@@ -0,0 +1,16 @@
+package com.mojio.mojiosdk.models;
+
+/**
+ * Created by ssawchenko on 15-02-06.
+ */
+public class Mojio {
+
+    public String OwnerId;
+    public String Name;
+    public String Imei;
+    public String LastContactTime;
+    public String VehicleId;
+    public String _deleted;
+    public String _id;
+
+}
