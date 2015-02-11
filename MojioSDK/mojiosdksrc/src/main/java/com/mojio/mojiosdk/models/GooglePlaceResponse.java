aa192a1cb342530266aca9e2a3276c1c563d941e
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2015-02-10 16:34:00 -0800

    Added Google Places response models

---
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/GooglePlaceResponse.java
@@ -0,0 +1,9 @@
+package com.mojio.mojiosdk.models;
+
+/**
+ * Created by ssawchenko on 15-02-10.
+ */
+public class GooglePlaceResponse {
+    public String status;
+    public GooglePlace[] results;
+}
