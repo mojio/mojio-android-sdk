2d0b1ac5ef48a675926ff95fe12e5c2c4cb33d7b
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2015-02-24 22:44:08 -0800

    Diagnostic model update
    * Added concept of "Instances" to Diagnostic data model so that we could pass along
      all times / locations for each instance of the DTC (History may have multiple instances
      of each code)

--- MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Diagnostics.java
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Diagnostics.java
@@ -1,31 +1,44 @@
 package com.mojio.mojiosdk.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by ssawchenko on 15-02-06.
  */
 public class Diagnostics {
 
-    // Note, commenting out parameters currently unused by our app
+    public static  class Instance {
+        public String Time;
+        public Location Location;
+
+        public Instance(String t, Location l) {
+            this.Time = t;
+            this.Location = l;
+        }
+    }
 
     public String Type;
     public String Code;
     public String Description;
     public String Source;
-    //public String _deleted;
+    public String Category;
+    public String Severity;
     public String _id;
 
     public int Count; // # of identical DTCs; not given in JSON, is set in AlertsFragment
+    public ArrayList<Instance> Instances; // Diagnostic events will have instances
 
     public Diagnostics() {
         this.Count = 1;
+        this.Instances = new ArrayList<>();
     }
 
     public Diagnostics(String... args) {
+        this();
         this.Code = args[0];
         this.Description = args[1];
         this.Source = args[2];
         this._id = args[3];
-        this.Count = 1;
     }
 
     public static Diagnostics[] createTestData() {
