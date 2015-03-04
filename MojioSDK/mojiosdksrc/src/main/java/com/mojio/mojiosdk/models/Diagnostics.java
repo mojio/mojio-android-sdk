e3f86f05d5dc43dfb84cb22867d6c3f290075383
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2015-03-04 12:13:09 -0800

    Removed the concept of "Instances" from Diagnostics
    * Instead VirtualMechanic now contains a DiagnosticInstances class.
    * This was removed because setting the instances here lead to volitile data caching.

--- MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Diagnostics.java
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Diagnostics.java
@@ -1,21 +1,9 @@
 package com.mojio.mojiosdk.models;
 
-import java.util.ArrayList;
-
 /**
  * Created by ssawchenko on 15-02-06.
  */
 public class Diagnostics {
-
-    public static  class Instance {
-        public String Time;
-        public Location Location;
-
-        public Instance(String t, Location l) {
-            this.Time = t;
-            this.Location = l;
-        }
-    }
 
     public String Type;
     public String Code;
@@ -25,13 +13,7 @@ public class Diagnostics {
     public String Severity;
     public String _id;
 
-    public int Count; // # of identical DTCs; not given in JSON, is set in AlertsFragment
-    public ArrayList<Instance> Instances; // Diagnostic events will have instances
-
-    public Diagnostics() {
-        this.Count = 1;
-        this.Instances = new ArrayList<>();
-    }
+    public Diagnostics() {}
 
     public Diagnostics(String... args) {
         this();
@@ -40,15 +22,4 @@ public class Diagnostics {
         this.Source = args[2];
         this._id = args[3];
     }
-
-    public static Diagnostics[] createTestData() {
-        Diagnostics[] result = new Diagnostics[4];
-        result[0] = new Diagnostics("P0200", "Your train has little power", "Powertrain", "00001");
-        result[1] = new Diagnostics("P0300", "Something about a car", "Powertrain", "00002");
-        result[2] = new Diagnostics("U0120", "The system is down", "Network", "00003");
-        result[3] = new Diagnostics("B1000", "Its almost bathing suit season", "Body", "00004");
-
-        return result;
-    }
-
 }
