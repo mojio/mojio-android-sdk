c62192584ec0103bcfb680321611c9c5780fd2e7
Mun Jian Liew <munjianliew@gmail.com>
2015-03-25 13:23:04 -0700

    add Tip model for the service tab

---
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/Tip.java
@@ -0,0 +1,12 @@
+package com.mojio.mojiosdk.models;
+
+/**
+ * Created by jian on 24/03/2015.
+ */
+public class Tip {
+
+    public String Date;
+    public String Time;
+    public String Title;
+    public String Details;
+}
