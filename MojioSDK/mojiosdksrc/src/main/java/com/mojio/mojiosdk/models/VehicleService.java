7e68126567df5a900b266276836a896ddd42f03e
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2015-04-01 11:56:35 -0700

    Added VehicleService model

---
+++ MojioSDK/mojiosdksrc/src/main/java/com/mojio/mojiosdk/models/VehicleService.java
@@ -0,0 +1,25 @@
+package com.mojio.mojiosdk.models;
+
+/**
+ * Created by ssawchenko on 15-04-01.
+ */
+public class VehicleService {
+
+    public int DOVehicleId;
+    public int DOEngineId;
+    public String TransNotes;
+    public String MaintenanceCategory;
+    public String MaintenanceName;
+    public String MaintenanceNotes;
+    public String ScheduleName;
+    public String ScheduleDescription;
+    public String OperatingParameter;
+    public String OperatingParameterNotes;
+    public String ComputerCode;
+    public String ServiceEvent;
+    public String IntervalType;
+    public Float Value;
+    public String Units;
+    public Float IntervalValue;
+
+}
