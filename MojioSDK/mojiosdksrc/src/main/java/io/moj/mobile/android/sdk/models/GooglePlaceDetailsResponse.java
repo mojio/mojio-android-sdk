33d91556a16b7ddfe9ae4ab80542728591ab067e
Stephen Kidson <stephenkidson@gmail.com>
2015-11-10 17:59:58 -0800

    Kill everything

--- MojioSDK/mojiosdksrc/src/main/java/io/moj/mobile/android/sdk/models/GooglePlaceDetailsResponse.java
+++
@@ -1,9 +0,0 @@
-package io.moj.mobile.android.sdk.models;
-
-/**
- * Created by ssawchenko on 15-02-10.
- */
-public class GooglePlaceDetailsResponse {
-    public String status;
-    public GooglePlaceDetails result;
-}
--- MojioSDK/mojiosdksrc/src/main/java/io/moj/mobile/android/sdk/models/Store.java
+++
@@ -1,9 +0,0 @@
-package io.moj.mobile.android.sdk.models;
-
-/**
- * Created by ssawchenko on 15-02-03.
- */
-public class Store {
-    public String Key;
-    public String Value;
-}
