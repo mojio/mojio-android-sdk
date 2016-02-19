717ca5187434ca46110afafeb2fd3d3a08ecb0e0
Stephen Kidson <stephenkidson@gmail.com>
2016-02-19 14:43:52 -0800

    Made Log.LogcatLogger a public static class so we can unit test it (and clients can use or extend however they want)

--- MojioSDK/mojio-sdk-common/src/test/java/io/moj/mobile/android/sdk/LogTest.java
+++ MojioSDK/mojio-sdk-common/src/test/java/io/moj/mobile/android/sdk/LogTest.java
@@ -2,17 +2,27 @@ package io.moj.mobile.android.sdk;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Random;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
+@PrepareForTest({android.util.Log.class})
+@RunWith(PowerMockRunner.class)
 public class LogTest {
 
     @Before
@@ -91,6 +101,60 @@ public class LogTest {
     @Test
     public void testE() {
         verifyLogLevel(android.util.Log.ERROR);
+    }
+
+    @Test
+    public void testLogcatLogger() {
+        String tag = "tag";
+        String msg = "msg";
+        Throwable t = new Throwable("Exception message");
+
+        PowerMockito.mockStatic(android.util.Log.class);
+        Mockito.when(android.util.Log.println(anyInt(), anyString(), anyString())).thenReturn(0);
+        Mockito.when(android.util.Log.getStackTraceString(t)).thenReturn(t.getMessage());
+
+        Log.LogcatLogger logger = new Log.LogcatLogger();
+        Log.append(logger);
+
+        Log.v(tag, msg);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.VERBOSE), eq(tag), eq(msg));
+
+        Log.v(tag, msg, t);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.VERBOSE), eq(tag), contains(t.getMessage()));
+
+        Log.d(tag, msg);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.DEBUG), eq(tag), eq(msg));
+
+        Log.d(tag, msg, t);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.DEBUG), eq(tag), contains(t.getMessage()));
+
+        Log.i(tag, msg);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.INFO), eq(tag), contains(msg));
+
+        Log.i(tag, msg, t);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.INFO), eq(tag), contains(t.getMessage()));
+
+        Log.w(tag, msg);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.WARN), eq(tag), contains(msg));
+
+        Log.w(tag, msg, t);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.WARN), eq(tag), contains(t.getMessage()));
+
+        Log.e(tag, msg);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.ERROR), eq(tag), contains(msg));
+
+        Log.e(tag, msg, t);
+        verifyStatic();
+        android.util.Log.println(eq(android.util.Log.ERROR), eq(tag), contains(t.getMessage()));
     }
 
     @SuppressWarnings("ThrowableInstanceNeverThrown")
