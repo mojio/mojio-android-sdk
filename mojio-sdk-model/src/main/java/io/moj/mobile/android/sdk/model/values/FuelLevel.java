b837d98cd651c95de6a56a81699c0c613cc1219e
Stephen Kidson <stephenkidson@gmail.com>
2016-03-30 11:23:57 -0700

    Added RiskSeverity field to FuelLevel

--- mojio-sdk-model/src/main/java/io/moj/mobile/android/sdk/model/values/FuelLevel.java
+++ mojio-sdk-model/src/main/java/io/moj/mobile/android/sdk/model/values/FuelLevel.java
@@ -1,12 +1,15 @@
 package io.moj.mobile.android.sdk.model.values;
 
 import io.moj.mobile.android.sdk.model.enums.FuelLevelUnit;
+import io.moj.mobile.android.sdk.model.enums.RiskSeverity;
 
 /**
  * Model object for a FuelLevel value.
  * Created by mhorie on 2016-01-14.
  */
 public class FuelLevel extends DeviceMeasurement {
+
+    private RiskSeverity RiskSeverity;
 
     public FuelLevelUnit getBaseFuelLevelUnit() {
         return FuelLevelUnit.fromKey(getBaseUnit());
@@ -24,8 +27,18 @@ public class FuelLevel extends DeviceMeasurement {
         setUnit(unit.getKey());
     }
 
+    public RiskSeverity getRiskSeverity() {
+        return RiskSeverity;
+    }
+
+    public void setRiskSeverity(RiskSeverity riskSeverity) {
+        RiskSeverity = riskSeverity;
+    }
+
     @Override
     public String toString() {
-        return "FuelLevel{} " + super.toString();
+        return "FuelLevel{" +
+                "RiskSeverity=" + RiskSeverity +
+                "} " + super.toString();
     }
 }
